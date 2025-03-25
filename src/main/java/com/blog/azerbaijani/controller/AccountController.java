7335d471029b9c7f677983be40040c43bb24e8af
Farid Aliyev <142145981+aliyevfarid135@users.noreply.github.com>
2025-03-26 00:26:47 +0400

    Update AccountController.java

--- src/main/java/com/blog/azerbaijani/controller/AccountController.java
+++ src/main/java/com/blog/azerbaijani/controller/AccountController.java
@@ -10,6 +10,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 import java.util.Random;
 
 @Controller
@@ -22,6 +25,13 @@ public class AccountController {
     @Autowired
     private UserRepository userRepository;
 
+    private final Random random = new Random();
+
+    private User getAuthenticatedUser() {
+        String authentication = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userRepository.findByUsername(authentication);
+    }
+
     @GetMapping("/create")
     public String createAccount() {
         return "createAccount";
@@ -29,19 +39,16 @@ public class AccountController {
 
     @PostMapping("/create")
     public String createAccount(Account account) {
-        String authentication = SecurityContextHolder.getContext().getAuthentication().getName();
-        User user = userRepository.findByUsername(authentication);
+        User user = getAuthenticatedUser();
         account.setUser(user);
 
-        String accountNumber = generateRandomNumber(16);
-        String cvvNumber = generateRandomNumber(3);
-        String date = generateRandomDate();
-        Double balance = 0.0;
-        account.setBalance(balance);
-        account.setAccountNumber(accountNumber);
-        account.setCvvNumber(cvvNumber);
-        account.setDate(date);
+        account.setAccountNumber(generateRandomNumber(16));
+        account.setCvvNumber(generateRandomNumber(3));
+        account.setDate(generateRandomDate());
+        account.setBalance(0.0); 
+
         accountRepository.save(account);
+
         user.setDefaultAccount(account);
         userRepository.save(user);
 
@@ -50,8 +57,7 @@ public class AccountController {
 
     @GetMapping("/default")
     public String getDefaultCardPage(Model model) {
-        String authentication = SecurityContextHolder.getContext().getAuthentication().getName();
-        User user = userRepository.findByUsername(authentication);
+        User user = getAuthenticatedUser();
         model.addAttribute("user", user);
         model.addAttribute("account", user.getDefaultAccount());
         return "card";
@@ -59,45 +65,30 @@ public class AccountController {
 
     @PostMapping("/default")
     public String chooseDefaultCard(@RequestParam Long id) {
-        String authentication = SecurityContextHolder.getContext().getAuthentication().getName();
-        User user = userRepository.findByUsername(authentication);
-        for (Account a : user.getAccountList()) {
-            if (Objects.equals(a.getId(), id)) {
-                user.setDefaultAccount(a);
-            }
-        }
+        User user = getAuthenticatedUser();
+        user.getAccountList().stream()
+            .filter(account -> Objects.equals(account.getId(), id))
+            .findFirst()
+            .ifPresent(user::setDefaultAccount);
+
         userRepository.save(user);
         return "redirect:default?success";
     }
 
     @GetMapping("/info")
     public String getCardInfo(Model model) {
-        String authentication = SecurityContextHolder.getContext().getAuthentication().getName();
-        User user = userRepository.findByUsername(authentication);
-        model.addAttribute(user.getDefaultAccount());
+        User user = getAuthenticatedUser();
+        model.addAttribute("account", user.getDefaultAccount());
         return "card";
     }
 
     private String generateRandomNumber(int length) {
-        Random random = new Random();
-        StringBuilder result = new StringBuilder();
-
-        for (int i = 0; i < length; i++) {
-            result.append(random.nextInt(10));
-        }
-
-        return result.toString();
+        return String.format("%0" + length + "d", random.nextInt((int) Math.pow(10, length)));
     }
 
     private String generateRandomDate() {
-        Random random = new Random();
-        int year = random.nextInt(10) + 2023;
-        int month = random.nextInt(12) + 1;
-        int day = random.nextInt(28) + 1;
-
-        SimpleDateFormat sdf = new SimpleDateFormat("MM/yyyy");
-        Date date = new Date(year - 1900, month - 1, day);
-
-        return sdf.format(date);
+        LocalDate date = LocalDate.of(random.nextInt(10) + 2023, random.nextInt(12) + 1, random.nextInt(28) + 1);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/yyyy");
+        return date.format(formatter);
     }
 }
