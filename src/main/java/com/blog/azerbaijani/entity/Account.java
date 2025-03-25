1b66939373452e70a716f1696b3422746b46c318
Farid Aliyev <142145981+aliyevfarid135@users.noreply.github.com>
2025-03-25 22:54:00 +0400

    Update Account.java

--- src/main/java/com/blog/azerbaijani/entity/Account.java
+++ src/main/java/com/blog/azerbaijani/entity/Account.java
@@ -7,9 +7,9 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Entity
-@Data//LOMBOK
-@NoArgsConstructor//LOMBOK
-@AllArgsConstructor//LOMBOK
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Account {
 
     @Id
